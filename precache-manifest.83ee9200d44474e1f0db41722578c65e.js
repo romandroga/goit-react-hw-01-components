59bc77c0b0cefccf971f22b76ad735e63f1e1ab8
Roman Droga <romandroga@gmail.com>
2020-04-04 00:33:20 +0300

    Updates

---
+++ precache-manifest.83ee9200d44474e1f0db41722578c65e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2748f60bb95cd47e05221be1e448732",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "05d6dbe3d7a96bf75ce6",
+    "url": "/goit-react-hw-01-components/static/css/main.49e6b26d.chunk.css"
+  },
+  {
+    "revision": "f5bb6cd1ce5c96caf170",
+    "url": "/goit-react-hw-01-components/static/js/2.50f5bc69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.50f5bc69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05d6dbe3d7a96bf75ce6",
+    "url": "/goit-react-hw-01-components/static/js/main.aca464d2.chunk.js"
+  },
+  {
+    "revision": "61ea0416005e4a073e74",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.60be8746.js"
+  }
+]);
